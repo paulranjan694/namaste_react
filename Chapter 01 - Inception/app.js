06db3f207b3e1b2c89bd27009611fe368ff705c6
Ranjan Paul <paulranjan694@gmail.com>
2022-12-24 23:50:17 +0530

    Added Nested Children to root using React.createElement api

--- Chapter 01 - Inception/app.js
+++ Chapter 01 - Inception/app.js
@@ -1,5 +1,10 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const h1 = React.createElement("h1",{className:"title"},"Namaste React!!"); // args in createElement api - element, props/attributes passed to the element, children (to be displayed/rendered inside element)
+const h2 = React.createElement("h1",{className:"title2"},"Nested H1 element"); // args in createElement api - element, props/attributes passed to the element, children (to be displayed/rendered inside element)
 
-root.render(h1);
+
+const container = React.createElement("div",{className:"container"},[h1,h2]); // args in createElement api - element, props/attributes passed to the element, children (to be displayed/rendered inside element)
+
+
+root.render(container);
